61caca0d8ff0be044b938f5a1d2d2b1082f3f0ec
takuma <mrbig8192@gmail.com>
2019-11-16 19:28:05 +0900

    環境設定変更・完了

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,83 +1,50 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var notify = require('gulp-notify');
 var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync');
 
 
-// Sassをコンパイルするタスクの設定
-gulp.task("css", function () {
-    return gulp.src('./hidari-kikino-eren/src/*.scss')// コンパイル対象のSassファイル
+// Sassコンパイル
+gulp.task('css', function () {
+    return gulp.src('./web/src/sass/*.scss')// コンパイル対象のSassファイル
             .pipe(sass()) // コンパイル実行
-            .pipe(autoprefixer()) // ベンダープレフィックスの付与
-            .pipe(gulp.dest('./hidari-kikino-eren/css')); // 出力
+            .pipe(autoprefixer({cascade: false})) // ベンダープレフィックスの付与
+            .pipe(gulp.dest('./web/asset/')); // 出力
 });
 
-// .min.cssを生成するタスクの設定
-gulp.task("mincss", function () {
-    return gulp.src('./hidari-kikino-eren/css')//上のタスクで出力したcssファイル
-            .pipe(cleanCSS()) // cssを圧縮
-            .pipe(rename({extname:'.min.css'})) // 名前を.min.cssにする
-            .pipe(gulp.dest('./hidari-kikino-eren')) // 出力
-            .pipe(notify({
-                title: 'minified',
-                message: new Date(),
-                sound: 'Pop',
-                icon: 'icon.png' // 適当なアイコンを追加してみてください
-            }));
+//css minify
+gulp.task('mincss', function () {
+  return gulp.src('./web/asset/*.css')
+              .pipe(cleanCSS()) // cssを圧縮
+              .pipe(rename({extname:'.min.css'})) // 名前を.min.cssにする
+              .pipe(gulp.dest('./web/asset/min/')) // 出力
 });
 
-gulp.task("browserSyncTask", function() {
+//Sassコンパイルとcss minify タスク
+gulp.task('watch',function(){
+  gulp.watch('./web/src/sass/*.scss', gulp.series('css','mincss'));
+})
+
+// browserSync リロード
+gulp.task('bs-reload', function (done) {
+  browserSync.reload();
+  done();
+});
+
+//browserSync
+gulp.task('browser-sync', function(done) {
   browserSync({
     server: {
-      baseDir: "./hidari-kikino-eren",
-      index  : "test.html" 
+        baseDir: "./web/",    //対象ディレクトリ
+        index  : "./data/test.html"    //インデックスファイル ここは適宜書き換える
     }
   });
-
-  // srcフォルダ以下のファイルを監視
-  gulp.watch("./hidari-kikino-eren/src/*.scss", function(done) {
-    browserSync.reload(); // ファイルに変更があれば同期しているブラウザをリロード
-    done();
-  });
+  gulp.watch('./web/data/*.html',gulp.task('bs-reload')); //監視対象
+  gulp.watch('./web/src/sass/*.scss',gulp.task('bs-reload')); //監視対象
+  gulp.watch('./web/src/js/*.js',gulp.task('bs-reload')); //監視対象
+  done();
 });
 
-
-gulp.task("default", function () {
-    // scssフォルダを監視し、変更があったらコンパイルする
-    gulp.watch('./hidari-kikino-eren/src/*.scss',gulp.series('css', 'mincss'));
-});
-
-
-/*
-
-
-gulp.task('sass', function () {
-  gulp.src('./hidari-kikino-eren/src/*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('./hidari-kikino-eren/'));
-});
-
-gulp.task('watch', function(){
-  gulp.watch('./hidari-kikino-eren/src/*.scss', sass);
-});
-
-gulp.task('browser-sync', function() {
-  browserSync({
-      server: {
-           baseDir: "./hidari-kikino-eren"       
-          ,index  : "test.html"      
-      }
-  });
-});
-
-gulp.task('bs-reload', function () {
-  browserSync.reload();
-});
-
-gulp.task('default', browserSync, function () {
-  gulp.watch('./hidari-kikino-eren/src/*.scss', browserReload);
-});
-*/
+gulp.task('default', gulp.parallel('browser-sync','watch'));
