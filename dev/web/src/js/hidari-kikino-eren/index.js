7cd55bc7a6f6bc6c003c59efc0e42ad853a96b41
takuma <mrbig8192@gmail.com>
2019-11-18 07:41:27 +0900

    opening 読み込み処理テスト中

--- dev/web/src/js/hidari-kikino-eren/index.js
+++ dev/web/src/js/hidari-kikino-eren/index.js
@@ -1,1 +1,23 @@
 console.log('test');
+
+
+// $('.js-test').addClass('add-load');
+
+
+var progress = 0;
+var imgCount = $('img').length;
+$("img").each(function(){
+  var src = $(this).attr('src');
+  $("<img>").attr('src',src).on('load',function(){
+    progress++;
+    console.log(progress);
+    
+  });
+});
+// setInterval(function(){
+//   console.log(progress);
+  
+// }, 1);
+// $(window).on('load' , function(){
+//   $('.mod-item1').addClass('add-load');
+// })
