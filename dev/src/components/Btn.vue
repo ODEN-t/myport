5c40d6e767874ac0a62997ad9e734de457d32214
k-takuma <xxzera@gmail.com>
2020-04-22 00:04:43 +0900

    vue 環境構築完了

---
+++ dev/src/components/Btn.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="_handleCount">
+        Count <slot></slot>!
+    </button>
+</template>
+
+<script>
+export default {
+    methods: {
+        _handleCount() {
+            this.$emit("handleCount");
+        }
+    }
+}
+</script>
+<style scoped>
+.button {
+  color: red;
+}
+</style>>
--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,9 +1,88 @@
-const pageName = "hidari-kikino-eren";  // 適宜書き換える
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-module.exports  = {
-  mode: "production",
-  entry: "./web/src/js/" + pageName + "/index.js",
+module.exports = {
+  mode: "development",
+  devtool: "#eval-source-map",
+  devServer: {
+    contentBase: "public",
+    open: true
+  },
+  performance: {
+    hints: false
+  },
+  entry: {
+    hello : "./src/main.js"
+  },  
   output: {
-    filename: "bundle.js"
-  }
+    path: `${__dirname}/public`, // 絶対パスでの指定
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // ローダーの対象から外す
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env"  // プリセットを指定することで、ES2019 を ES5 に変換
+            ]
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {
+            scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js"
+    },
+    extensions: ["*", ".js", ".vue", ".json"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new VueLoaderPlugin()
+  ]
 }
